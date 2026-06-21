7254591056d0da92a6b5460b7b0ec703cd22aad8
Atikur Rahman <ar.atik1997@gmail.com>
2026-06-21 21:52:22 +0600

    add student creation with spread operator

--- bin/cli_project.dart
+++ bin/cli_project.dart
@@ -8,6 +8,8 @@ void main()
 
     int? menunav;
 
+    var students = <Map<String, dynamic>>[];
+
     
     do
     {
@@ -27,6 +29,50 @@ void main()
         var menunavstr = stdin.readLineSync();
 
         menunav = int.tryParse(menunavstr ?? '') ?? 0;
+
+        switch (menunav)
+        {
+            case 1:
+                {
+                    print("Enter student name: ");
+                    var name = stdin.readLineSync() ?? "";
+                    var student = {
+                        "name": name,
+                        "subjects": {...subjects},
+                        "scores": <String, double>{},
+                        "bonus": 0.0,
+                        "Average": 0.0,
+                        "Grade": "",
+                        "comments": <String>[],
+                    };
+                    students.add(student);
+                    print("Student added successfully.");
+                }
+                break;
+            case 2:
+                print("Record Score");
+                break;
+            case 3:
+                print("Add Bonus Profile");
+                break;
+            case 4:
+                print("Add Comment");
+                break;
+            case 5:
+                print("View All Student");
+                break;
+            case 6:
+                print("View Report Card");
+                break;
+            case 7:
+                print("Class Summary");
+                break;
+            case 0:
+                print("Exiting...");
+                break;
+            default:
+                print("Invalid option. Please try again.");
+        }
 
     }
     while (menunav != 0);
